96929fb6a2a6cfaa17fee173a36b8530c1f3e430
xyyy1719 <xyyy1719@outlook.com>
2016-10-20 08:50:15 +0800

    first commit

---
+++ gorilla-routing/routing.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	fileName := "files/" + pageID + ".html"
+	info, err := os.Stat(fileName)
+	if err != nil {
+		fileName = "error/404.html"
+	}
+	http.ServeFile(w, r, fileName)
+}
+
+func main() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	http.Handle("/", rtr)
+	http.ListenAndServe(PORT, nil)
+}
