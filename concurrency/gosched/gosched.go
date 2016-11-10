354aa62c664b78d8806d2f6cd45e9b0a665bee00
xyyy1719 <xyyy1719@outlook.com>
2016-11-10 12:26:12 +0800

    update comment for runtime.Gosched()

--- concurrency/gosched/gosched.go
+++ concurrency/gosched/gosched.go
@@ -12,6 +12,8 @@ func main() {
 		fmt.Println("Inside a goroutine.")
 	}()
 	fmt.Println("Outside again.")
-	runtime.Gosched() // 通知Go Runtime运行其他goroutine
+	// 通知Go Runtime运行其他goroutine，但并不会block主程序的运行
+	// 一旦主程序运行完毕，其他goroutine的程序也不会继续执行下去
+	runtime.Gosched()
 	fmt.Println("bye")
 }
